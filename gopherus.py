6f67d3e4d27a74e24516ed52c2bfedf1405c99f2
ribeirin <ribeirin@localhost.localdomain>
2024-12-21 14:42:56 -0300

    refactoring to python3 usage

--- gopherus.py
+++ gopherus.py
@@ -1,8 +1,7 @@
 import argparse
 import sys
 sys.path.insert(0, './scripts/')
-#from scripts import FastCGI, MySQL, PostgreSQL, DumpMemcached, PHPMemcached, PyMemcached, RbMemcached, Redis, SMTP, Zabbix
-from scripts import Redis, MySQL
+from scripts import FastCGI, MySQL, PostgreSQL, DumpMemcached, PHPMemcached, PyMemcached, RbMemcached, Redis, SMTP, Zabbix
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--exploit",
@@ -41,23 +40,23 @@ if not args.exploit:
 
 if args.exploit == "mysql":
     MySQL.MySQL()
-# elif args.exploit == "postgresql":
-#     PostgreSQL.PostgreSQL()
-# elif args.exploit == "fastcgi":
-#     FastCGI.FastCGI()
+elif args.exploit == "postgresql":
+    PostgreSQL.PostgreSQL()
+elif args.exploit == "fastcgi":
+    FastCGI.FastCGI()
 elif args.exploit == "redis":
     Redis.Redis()
-# elif args.exploit == "smtp":
-#     SMTP.SMTP()
-# elif args.exploit == "zabbix":
-#     Zabbix.Zabbix()
-# elif args.exploit == "dmpmemcache":
-#     DumpMemcached.DumpMemcached()
-# elif args.exploit == "phpmemcache":
-#     PHPMemcached.PHPMemcached()
-# elif args.exploit == "rbmemcache":
-#     RbMemcached.RbMemcached()
-# elif args.exploit == "pymemcache":
-#     PyMemcached.PyMemcached()
+elif args.exploit == "smtp":
+    SMTP.SMTP()
+elif args.exploit == "zabbix":
+    Zabbix.Zabbix()
+elif args.exploit == "dmpmemcache":
+    DumpMemcached.DumpMemcached()
+elif args.exploit == "phpmemcache":
+    PHPMemcached.PHPMemcached()
+elif args.exploit == "rbmemcache":
+    RbMemcached.RbMemcached()
+elif args.exploit == "pymemcache":
+    PyMemcached.PyMemcached()
 else:
     parser.print_help()
